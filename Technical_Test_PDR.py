969cf0d98bc698a7fd46c39f8328b6c13a068e39
peter-d-russell <peter_russell@talktalk.net>
2021-01-16 09:44:48 +0000

    added summary/detail paramter

--- Technical_Test_PDR.py
+++ Technical_Test_PDR.py
@@ -3,7 +3,15 @@
 Technical_Test - Find average number of words by artist.
 
 Finds the average number of words in songs by an artist.
-If the names of 2 artists are passed then a comparison is performed.
+The names of 2 artists can be passed for comparison.
+
+Example Command: python Technical_Test_PDR "D" "Neil Young" "The Pixies"
+
+The first parameter can be "D" or "S"
+    D - Detailed report including retrieved song names and number of words
+    S - Summary report with just average number number of words per song
+
+Subsequent parameters have either one or two artists names.
 
 Need to do 
 pip install musicbrainxngs
@@ -20,27 +28,31 @@ import json
 
 numargs = len(sys.argv)
 
-if 2 <= numargs <= 3:
-    artist_1 = sys.argv[1]
-    if numargs == 3:
-        artist_2 = sys.argv[2]
+if 3 <= numargs <= 4:
+    artist_1 = sys.argv[2]
+    if numargs == 4:
+        artist_2 = sys.argv[3]
 else:
     print("Invalid number of arguments passed.")
     quit()
 
-def get_artist_id(artist):
+if sys.argv[1] !="S" and sys.argv[1] !="D":
+    print("First parameter has to be S or D")
+    quit()
 
-  musicbrainzngs.set_useragent(app="technical_test", version=1.0, contact="none")
-
-  artist = musicbrainzngs.search_artists(artist=artist_1)
-
-  scores = []
-  score = {}
-  count = 0
+musicbrainzngs.set_useragent(app="technical_test", version=1.0, contact="none")
 
 # Obtain list of artists which match the name passed as a parameter
 # There often several artists whose name matches - attempt to find the most popular one
 # Sort the list and obtain the ID for the one with the biggest ext:score
+
+def get_artist_id(artist):
+
+  artist = musicbrainzngs.search_artists(artist=artist)
+
+  scores = []
+  score = {}
+  count = 0
 
   for obj in artist["artist-list"]:
     count+=1
@@ -59,7 +71,8 @@ def get_artist_id(artist):
   artist_name=first_dict["artist"]
 
 
-# Obtain lyrics for a song and count the words
+# Obtain lyrics for a song and count the words and number of songs
+# in global variables.
 
 def get_lyrics(artist, title):
   r = requests.get("https://api.lyrics.ovh/v1/" + str(artist) + "/" + str(title) )
@@ -69,12 +82,15 @@ def get_lyrics(artist, title):
   lyrics=text["lyrics"]
   num_of_words = len(lyrics.split())
   if num_of_words > 0:
-    #print(title)
-    #print(num_of_words)
+    if sys.argv[1] == "D":
+        print(str(num_of_words) + " : " + title)
     global artist_songs
     global artist_words
     artist_songs+=1
     artist_words+=num_of_words
+
+
+# Get track lists from albums identified
 
 def get_tracks(artist, album):
     result = musicbrainzngs.search_releases(artist=artist, release=album, limit=1)
@@ -85,13 +101,17 @@ def get_tracks(artist, album):
     for x in range(len(t)):
         line = (t[x])
         get_lyrics(artist, line["recording"]["title"])
-   
+
+
+# Get average song length for an artist by identifying album releases
+# then call other functions to get songs and lyrics.
+
 def get_song_length(artist_id):
   result = musicbrainzngs.get_artist_by_id(artist_id, includes=["release-groups"], release_type=["album"])
   for release_group in result["artist"]["release-group-list"]:
     get_tracks(artist_name, release_group["title"])
   
-get_artist_id(sys.argv[1])
+get_artist_id(sys.argv[2])
 
 artist_songs = 0
 artist_words = 0
@@ -104,8 +124,9 @@ print("Artist " + artist_1 + "  -  " + str("{:.2f}".format(artist_words_per_song
 print(str(artist_songs) + " Songs")
 print(str(artist_words) + " Words")
 
-if sys.argv[2] != ' ': 
-  get_artist_id(sys.argv[2])
+if numargs == 4:
+  artist_id = " "
+  get_artist_id(sys.argv[3])
 
   artist_songs = 0
   artist_words = 0
@@ -118,72 +139,6 @@ if sys.argv[2] != ' ':
  
   print(str(artist_songs) + " Songs")
   print(str(artist_words) + " Words")
-
-#recordings = musicbrainzngs.search_works(arid=artist_id)
-#print(recordings)
-
-#for recobj in recordings["work-list"]:
-#       print(recobj["title"])
- 
-
-'''
-
-artis
-ts = os.listdir("./files/")
-results = [] ;
-for artist in artists:
-  tot_songs = 0
-  tot_words = 0 
-  songs = os.listdir("./files/" + artist )
-  for song in songs:
-    words = open("./files/" + artist + "/" + song )
-    num_of_words = len(words.read().split())
-    print("Song: " + song + " has " + str(num_of_words) + " words." )  
-    tot_songs += 1
-    tot_words += num_of_words
-  tot_words=7
-  average = "{:.3f}".format(tot_words / tot_songs)
-  print(str(tot_words))
-  #print(str("{:.4f}".format(average)))
-  print("Artist: " + artist + " has " + str(tot_words / tot_songs) + " words per song." ) 
-  result = {"Artist" : artist , "WordsPerSong" : average }
-  results.append(result)
-print(results)
-
-class Vehicle:
-  make = ' '
-  model = ' '
-  wheels = 0
-
-class Car(Vehicle):
-  def __init__(self, make, model):
-    self.wheels = 2
-    self.make = make
-    self.model = model
-
-  def change_wheels(self, wheels):
-    self.wheels = wheels
-
-mycar = Car(make="Vauxhall", model="Zafira")
-
-print(mycar.wheels)
-mycar.change_wheels(7)
-print(mycar.wheels)
-
-def Printout():
-  global a
-  a=1
-  print('********')
-Printout()
-
-print("a=" + str(a))
-
-
-'''
-
-
-
-  
 
 
 
